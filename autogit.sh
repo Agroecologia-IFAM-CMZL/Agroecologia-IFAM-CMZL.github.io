1aac35077085873c73949c4476ab9bd0bcd1ab74
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-11-29 15:22:55 -0300

    update: scripts-news.js integration successfully

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "update: scripts-news.js worked"
+git commit -m "update: scripts-news.js integration successfully"
 git push -u origin main
 git status
--- pages/scripts-news.js
+++ pages/scripts-news.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:8080/users/41f50e0a-290c-4759-abcb-53c5f45e5a25', {
+fetch('http://localhost:8080/users/{id}/news', {
     method: 'GET',
     mode: 'cors',
     headers: {
@@ -12,34 +12,37 @@ fetch('http://localhost:8080/users/41f50e0a-290c-4759-abcb-53c5f45e5a25', {
 
     return response.json();
 }).then(response => {
-    // Handle the JSON data
-    // Get the HTML element where you want to display the response
-    var responseElement = document.getElementById('test-accordion');
+    for(var count = 0; count < response.length; count++) {
 
-    var htmlContent = `
-    <div class="accordion-item">
-        <h2 class="accordion-header">
-            <button class="accordion-button collapsed text-bg-success p-3" 
-                type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" 
-                aria-expanded="false" aria-controls="collapseFour"
-            >
-                ${response.news[0].title}
-            </button>
-        </h2>
+        var responseElement = document.createElement('div');
+        var htmlContent = `
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-bg-success p-3" 
+                    type="button" data-bs-toggle="collapse" data-bs-target="#collapse${count}" 
+                    aria-expanded="true" aria-controls="collapse${count}"
+                >
+                    ${response[count].title}
+                </button>
+            </h2>
 
-        <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-                <strong>${response.news[0].caption}</strong> ${response.news[0].paragraphInit} <code>code</code>, ${response.news[0].paragraphMiddle} <br>
-                <a href="./news/news-exemplo03.html" class="link-success"> Saiba Mais </a>
+            <div id="collapse${count}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>${response[count].caption}</strong> ${response[count].paragraphInit} <code>code</code>, ${response[count].paragraphMiddle} <br>
+                    <a href="./news/news-example.html" class="link-success"> Saiba Mais </a>
+                </div>
             </div>
         </div>
-    </div>
-    `;
+        `;
 
-    // Set the HTML content
-    responseElement.innerHTML = htmlContent;
+        // Set the HTML content
+        responseElement.innerHTML = htmlContent;
 
-    console.log(JSON.stringify(response));
+        // get inside a div
+        var divAccordion = document.getElementById('accordionExample');
+
+        divAccordion.appendChild(responseElement);
+    }
 }).catch(error => {
     // Handle any errors
     console.error('Error:', error);
